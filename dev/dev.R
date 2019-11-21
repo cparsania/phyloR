dff729f26e34cd71cd34481cb610f8ff628adbf6
Chirag <chirag.parsania@gmail.com>
2019-11-21 19:22:17 +0800

    pkg taxizedb added to DESCRIPTION file

--- dev/dev.R
+++ dev/dev.R
@@ -1,7 +1,7 @@
 usethis::use_package("dplyr" ,min_version = "0.8.3")
-
 usethis::use_package("tibble" ,min_version = "2.1.3")
 usethis::use_package("taxize" ,min_version = "0.9.91")
+usethis::use_package("taxizedb" ,min_version = "0.1.9.913")
 
 devtools::document()
 
